186cbaf68562d22c8801fd76b52a30df8021f52d
Celine Hernandez <celine.hernandez@isb-sib.ch>
2012-12-18 15:28:25 +0100

    Test fork between all multiple testing correction methods.

---
+++ R/mtc_all_multiple_testing_correctionl.R
@@ -0,0 +1,50 @@
+sapply(
+    list.files(file.path(getwd(), 'R'), pattern="mtc_[^a][^l]{2}.+\\.R", full.names=TRUE),
+    source
+)
+
+#' @title Multiple testing correction
+#' 
+#' @description Apply multiple testing correction on the outcome of statistical tests.
+#' 
+#' @details
+#' When testing multiple times a null hypothesis, incorrect rejection is more 
+#' likely to occur when considering all tests together. Several methods have 
+#' been developped in order to correct for this problem.
+#' See also http://en.wikipedia.org/wiki/Multiple_comparisons
+#' 
+#' @param mtcMethod A label indicating which method should be applied.
+#'   Available methods include:
+#'     bh : Benjamini-Hochberg FDR
+#' @param matrixData Object containg the p-values to correct
+#'   Any data.frame containing a column entitled "p.values" containing values 
+#'   to be adjusted. Other columns should include "fold.change", can include 
+#'   "significant".
+#' @param outputFolderTemp Where the temporary files will be created
+#' @param outputFileNameRmd Where report paragraphs will be written
+#' @param thresholdPVal FDR threshold
+#' @return A data frame with data in matrixData (p.values, fold.change, 
+#'   significant) and the corrected p-values. Or NULL if no 
+applyMultipleTestingCorrection <- function(mtcMethod, 
+                                           matrixData, 
+                                           outputFolderTemp, 
+                                           outputFileNameRmd, 
+                                           thresholdPVal) {
+    
+    mtcPVal <- switch(
+        mtcMethod,
+        bh = applyAndReportBH(matrixData, outputFolderTemp, 
+                              outputFileNameRmd, thresholdPVal),
+        stop(
+            paste0(
+                c(
+                    "Unknown method in applyMultipleTestingCorrection call: \'", 
+                    mtcMethod,
+                    "\'."
+                ), 
+                sep=''), 
+            call=TRUE)
+    )
+    
+    return(mtcPVal)
+}
