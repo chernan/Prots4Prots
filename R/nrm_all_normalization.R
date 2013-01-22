43e441193d78a2684523dbfd6109fc374a0416a6
Celine Hernandez <celine.hernandez@isb-sib.ch>
2013-01-22 18:19:57 +0100

    Add method to run all normalizations + tests.

---
+++ R/nrm_all_normalization.R
@@ -0,0 +1,60 @@
+#' Normalization
+#' 
+#' Normalization is the process of making different samples comparable by 
+#' adjusting them to a common scale.
+#' 
+#' @seealso http://en.wikipedia.org/wiki/Normalization_%28statistics%29
+#' 
+sapply(
+    list.files(file.path(getwd(), 'R'), 
+               pattern="nrm_[^a][^l]{2}.+\\.R", 
+               full.names=TRUE),
+    source
+)
+
+
+#' @title Normalization methods.
+#' 
+#' @description Normalization methods.
+#' 
+#' @details
+#' Apply normalization methods on intensity data. Transform data in log-scale. 
+#' Stop if given an incorrect method.
+#' 
+#' @param normMethod A label indicating which method should be applied.
+#'   Available methods include:
+#'    log2 : log2 transformation (for comparison)
+#'    iqr : inter-quartile range normalization
+#'    vsn : variance stabilizing normalization
+#'    vsn05 : variance stabilizing normalization (robust to outliers)
+#' @param dataset A ExpressionSet with intensities to normalize.
+#' @param outputFolderTemp Where the temporary files will be created
+#' @param outputFileNameRmd Where report paragraphs will be written
+#' @return A data frame with normalized data (same format as input)
+applyNormalization <- function(normMethod, dataset, 
+                               outputFolderTemp, outputFileNameRmd) {
+    
+    outPVal <- switch(
+        normMethod,
+        log2 = applyAndReportLog2(exprs(dataset), 
+                                  outputFolderTemp, outputFileNameRmd),
+        iqr = applyAndReportIQR(exprs(dataset), 
+                                outputFolderTemp, outputFileNameRmd),
+        vsn = applyAndReportVSN(exprs(dataset), 
+                                outputFolderTemp, outputFileNameRmd),
+        vsn05 = applyAndReportVSN(exprs(dataset), 
+                                  outputFolderTemp, outputFileNameRmd, 
+                                  useRobustFit=TRUE),
+        stop(
+            paste0(
+                c(
+                    "Unknown method in applyNormalization call : \'", 
+                    normMethod,
+                    "\'."
+                ), 
+                sep=''), 
+            call=TRUE)
+    )
+    
+    return(outPVal)
+}
---
+++ inst/unitTests/test_nrm_all_normalization.R
@@ -0,0 +1,53 @@
+
+test_fun_applyNormalization <- function() {
+
+    ## Setup data
+    normalData <- replicate(8, rnorm(n=3000, mean=11.5, sd=2.2))
+    normalData <- 2^normalData
+    pData <- data.frame(Design=c(rep("Experiment",4), rep("Control",4)))
+    rownames(pData) <- c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8" )
+    
+    dataSet <- new("ExpressionSet", exprs = normalData, 
+                   phenoData = new("AnnotatedDataFrame",pData))
+    
+    
+    # Test
+    applyNormalization(
+        'log2',
+        dataSet, 
+        file.path(getwd(), 'temp'), 
+        file.path(getwd(), 'temp', 'out_test_nrm_applyNormalization.Rmd')
+    )
+    
+    applyNormalization(
+        'iqr',
+        dataSet, 
+        file.path(getwd(), 'temp'), 
+        file.path(getwd(), 'temp', 'out_test_nrm_applyNormalization.Rmd')
+    )
+
+    applyNormalization(
+        'vsn',
+        dataSet, 
+        file.path(getwd(), 'temp'), 
+        file.path(getwd(), 'temp', 'out_test_nrm_applyNormalization.Rmd')
+    )
+    
+    applyNormalization(
+        'vsn05',
+        dataSet, 
+        file.path(getwd(), 'temp'), 
+        file.path(getwd(), 'temp', 'out_test_nrm_applyNormalization.Rmd')
+    )
+    
+    checkException(
+        applyNormalization(
+            '',
+            dataSet, 
+            file.path(getwd(), 'temp'), 
+            file.path(getwd(), 'temp', 'out_test_nrm_applyNormalization.Rmd')
+            ),
+        silent = TRUE
+    )
+    
+}
