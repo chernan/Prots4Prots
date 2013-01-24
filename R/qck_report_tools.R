7b0e5f90d0da3a49d6075f9b818359382fbd3c59
Celine Hernandez <celine.hernandez@isb-sib.ch>
2013-01-24 14:41:13 +0100

    Added a linear regression function to QCK tools. + tests

--- R/qck_report_tools.R
+++ R/qck_report_tools.R
@@ -1,6 +1,7 @@
 #' Download latticeExtra
 #' sudo R CMD INSTALL latticeExtra_0.6-24.tar.gz
 library(latticeExtra)
+library("ggplot2")
 
 ## -----------------------------------------------------------------------------
 ## Tests / plots used in the report
@@ -42,4 +43,48 @@ displayHeatmap <- function(dataset, distMethod, title="Heatmap") {
     return(p)
 }
 
+#' @title Linear regression
+#' 
+#' @description Linear regression
+#' 
+#' @details 
+#' Display a linear regression between two columns of a dataset.
+#' 
+#' @param dataset A data.frame of intensities.
+#' @param indexX Column index for X values.
+#' @param indexY Column index for Y values.
+#' @param annX Legend position on X.
+#' @param annY Legend position on Y.
+#' @param show00 Should axis be shown? 
+displayLinearReg <- function(dataset, 
+                             indexX=1, indexY=2, 
+                             annX=10, annY=19, show00=FALSE) {
+    
+    model <- lm(dataset[,indexY]~dataset[,indexX])
+    
+    eq <- paste(
+        c(
+            'y = ', 
+            format(coef(model)[1], digits=2),
+            ifelse((coef(model)[2]>=0), ' + ', ' - '),
+            format(abs(coef(model)[2]), digits=3),
+            ' . x'
+        ), collapse=''
+    )    
+    r2val <- paste( c('r2 = ', format(summary(model)$r.squared, digits=3) ), 
+                    collapse='')    
+    p <- ggplot(data=dataset, 
+                aes_string(x=names(dataset)[indexX], y=names(dataset)[indexY]))
+    if(isTRUE(show00)) {
+        p <- p + geom_point() + 
+            geom_hline(yintercept=0, colour="grey") + 
+            geom_vline(xintercept=0, colour="grey")
+    }
+    p <- p + geom_point() + 
+        geom_smooth(method=lm, formula=y~x, se=FALSE, color="blue") +
+        annotate("text", x=annX, y=annY+0.2, hjust=0, vjust=0, label=eq) + 
+        annotate("text", x=annX, y=annY-0.2, hjust=0, vjust=1, label=r2val) 
+    
+    return(p)
+}
 
--- inst/unitTests/test_qck_report_tools.R
+++ inst/unitTests/test_qck_report_tools.R
@@ -10,3 +10,16 @@ test_gra_displayHeatmap <- function() {
     print(displayHeatmap(dataset, "euclidean"))
     
 }
+
+test_gra_displayLinearReg <- function() {
+    
+    ## Setup data
+    dataset <- data.frame(replicate(2, rnorm(n=500, mean=11, sd=2.5)))
+    
+    ## Run function... as test?
+    print(displayLinearReg(dataset))
+    print(displayLinearReg(dataset, 1, 2))
+    print(displayLinearReg(dataset, 1, 2, 4, 16))
+    print(displayLinearReg(dataset, 1, 2, 2, 3, TRUE))
+    
+}
